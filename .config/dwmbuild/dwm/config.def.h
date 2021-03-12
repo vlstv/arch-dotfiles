c4a16882b02380f5e238ff74c3161f2dbb589bfd
vlstv <svolstov@gmail.com>
2021-03-12 23:45:48 +0200

    dwm updates

--- .config/dwmbuild/dwm/config.def.h
+++ .config/dwmbuild/dwm/config.def.h
@@ -3,25 +3,26 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#202745";
-static const char col_gray2[]       = "#202745";
-static const char col_gray3[]       = "#969cb3";
-static const char col_gray4[]       = "#202745";
-static const char col_back1[]       = "#638cc4";
-static const char col_cyan[]        = "#76C8D9";
+static const char *fonts[]          = { "Hack Nerd Font:size=9.5" };
+static const char dmenufont[]       = "Hack Nerd Font:size=9.5";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#2e3440";
+static const char col_gray3[]       = "#d8dee9";
+static const char col_gray4[]       = "#2e3440";
+static const char col_back1[]       = "#81a1c1"; /*"#9c6279" dust rose*/
+static const char col_cyan[]        = "#81a1c1"; /*"#638cc4 "; old from i3 conf: #76C8D9*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -36,23 +37,29 @@ static const Rule rules[] = {
 	*	WM_CLASS(STRING) = instance, class
 	*	WM_NAME(STRING) = title
 	*/
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask    switchtag   isfloating   isterminal   noswallow   monitor */
+	{ "Steam",     NULL,       NULL,       1 << 5,      0,          0,           0,           0,          -1 },
+    { "firefox",   NULL,       NULL,       1 << 1,      1,          0,           0,           1,          -1 },
+	{ "Telegram",  NULL,       NULL,       1 << 2,      1,          0,           0,           0,          -1 },
+	{ "kitty",     NULL,       NULL,       0,           0,          0,           1,           0,          -1 },
+	{ "ranger",    NULL,       "ranger",   0,           0,          0,           1,           0,          -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #include "layouts.c"
+#include "gaplessgrid.c"
+#include "movestack.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "HHH",      grid },
+	{ "HHH",      gaplessgrid },
 };
 
 /* key definitions */
@@ -68,38 +75,49 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_back1, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
-#include "movestack.c"
+#include <X11/XF86keysym.h>
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Left,   focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_Up,     setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_Down,   setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Up,     setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Down,   setmfact,       {.f = -0.05} },
 	{ MODKEY|ShiftMask,             XK_Right,  movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_Left,   movestack,      {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_Left,   movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_s,      incrgaps,       {.i = +5 } },
 	{ MODKEY,                       XK_w,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,             XK_w,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,XF86XK_AudioRaiseVolume,    spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +10%; kill -RTMIN+11 $(pidof dwmblocks)") },
+	{ 0,XF86XK_AudioLowerVolume,    spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -10%; kill -RTMIN+11 $(pidof dwmblocks)") },
+	{ 0,XF86XK_AudioMute,           spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -RTMIN+11 $(pidof dwmblocks)") },
+	{ 0,XF86XK_AudioMicMute,        spawn,          SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle") },
+	{ 0,XF86XK_MonBrightnessUp,     spawn,          SHCMD("light -A 10") },
+	{ 0,XF86XK_MonBrightnessDown,   spawn,          SHCMD("light -U 10") },
+	{ 0,XK_Print,                   spawn,          SHCMD("scrot 'screenshot_%Y%m%d_%H%M%S.png'") },
+	{ ShiftMask,XK_Print,           spawn,          SHCMD("scrot -s 'screenshot_%Y%m%d_%H%M%S.png'") },
+	{ MODKEY|ShiftMask,XK_d,        spawn,          SHCMD("echo -e 'HDMI-1\neDP-1\nHDMI-1+eDP-1' | dmenu | /home/vlstv/s/tooglemonitors.sh")},
+	{ MODKEY,XK_p,                  spawn,          SHCMD("kitty -e ranger")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
--- .config/dwmbuild/dwm/config.h
+++ .config/dwmbuild/dwm/config.h
@@ -15,8 +15,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9", "TerminessTTF Nerd Font:size=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=9";
+static const char *fonts[]          = { "Hack Nerd Font:size=9.5" };
+static const char dmenufont[]       = "Hack Nerd Font:size=9.5";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#2e3440";
 static const char col_gray3[]       = "#d8dee9";
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 	*/
 	/* class      instance    title       tags mask    switchtag   isfloating   isterminal   noswallow   monitor */
 	{ "Steam",     NULL,       NULL,       1 << 5,      0,          0,           0,           0,          -1 },
-    { "firefox",   NULL,       NULL,       1 << 1,      1,          0,           0,           0,          -1 },
+    { "firefox",   NULL,       NULL,       1 << 1,      1,          0,           0,           1,          -1 },
 	{ "Telegram",  NULL,       NULL,       1 << 2,      1,          0,           0,           0,          -1 },
 	{ "kitty",     NULL,       NULL,       0,           0,          0,           1,           0,          -1 },
 	{ "ranger",    NULL,       "ranger",   0,           0,          0,           1,           0,          -1 },
@@ -97,6 +97,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
